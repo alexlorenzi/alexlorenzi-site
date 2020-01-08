d873f2d45ab65a74eb695b8dabfe5cd2461d17b7
Alex Lorenzi <alex.lorenzi@ensemble.com>
2020-01-08 13:01:49 -0500

    Added sitedeploy script to make pushing updates easier

---
+++ deploy.js
@@ -0,0 +1,11 @@
+const sitedeploy = require('gulp-bitbucket-site-deploy');
+const project = require('./package.json');
+
+sitedeploy({
+	username: 'alexlorenzi',
+	version: project.version,
+	source: './dist',
+	host: 'github.com',
+	ssh: true,
+	delete_paths: ['!CNAME'],
+});
--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-import { Remarkable } from "remarkable";
-import resume from "./assets/cv.md";
-import "./styles/site.scss";
+import { Remarkable } from 'remarkable';
+import resume from './assets/cv.md';
+import './styles/site.scss';
 
-const resume_content = document.getElementById("resume");
+const resume_content = document.getElementById('resume');
 
 // Link scroll to resume
-document.querySelector("#link__resume").addEventListener("click", e => {
-  e.preventDefault();
-  resume_content.scrollIntoView();
+document.querySelector('#link__resume').addEventListener('click', e => {
+	e.preventDefault();
+	resume_content.scrollIntoView();
 });
 
 // Render Resume
-const md = new Remarkable("default", { html: true });
-resume_content.querySelector(".content").innerHTML = md.render(resume);
+const md = new Remarkable('default', { html: true });
+resume_content.querySelector('.content').innerHTML = md.render(resume);
